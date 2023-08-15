18c8e2f8e0f262a68654e64cb09d9ec6b400bfd6
陈旭 <chenxu9741@cvte.com>
2023-08-15 08:21:43 +0800

    feat(v1.0.0): 初始化项目，提交一个空的可运行的第一版项目

---
+++ src/main/java/com/opensource/easyddd/EasyDddApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.easyddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyDddApplication.class, args);
+    }
+
+}
